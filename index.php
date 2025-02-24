5b6374db7575ec56d1ffc0b063be8c0e2325bcd1
1112211012 <1112211012@gap.wzu.edu.tw>
2025-02-24 15:32:04 +0800

    變更為php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>text</title>
+</head>
+<body>
+    <h1>我的練習 php & mysql</h1>
+
+    <p>今天的日期是：<?= date("Y年m月d日") ?></p>
+    <hr>
+    <p>我的身高: <?= $h165 ?></p>
+    <p>我的身高: <?= $w65 ?></p>
+    <p>我的BMI值: <?= $w/($h/100*$h/100) ?></p>
+    
+</body>
+</html>
