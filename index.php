9112bf85fe9cec50df5ab182091996d6579385f1
1112211012 <1112211012@gap.wzu.edu.tw>
2025-03-10 14:39:48 +0800

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,28 @@
     <title>活動報名表</title>
 </head>
 <body>
-    
+    <h1>活動報名表</h1>
+    <form action="">
+
+    <fieldest>
+    <legend>基本資料<legend>
+    <p>
+        <label for="name">姓名</label>
+        <input type="text" name="name" id="name" value="">
+    </p>
+        <fieldest>    
+
+    <fieldset>
+        <legend>使用行為</legend>
+    </fieldset>
+
+    <fieldset>
+        <legend>滿意度</legend>
+    </fieldset>
+
+    <fieldset>
+        <legend>資料上傳</legend>
+    </fieldset>
+    </form>
 </body>
 </html>
