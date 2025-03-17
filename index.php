62fb0d35b18ebf6711ad0e0b0757c8d16e9b479b
1112211012 <1112211012@gap.wzu.edu.tw>
2025-03-17 15:38:33 +0800

    Update index.php

--- index.php
+++ index.php
@@ -33,16 +33,16 @@
         <input type="text" name="phone" id="phone">
     </p>
     </fieldset>
-  <p>
-    <label for="place">居住區域</label>
-    <select name="place" id="place">
-        <option value="0">請選擇...</option>
-        <option value="1">北部</option>
-        <option value="2">中部</option>
-        <option value="3">南部</option>
-        <option value="4">東部</option>
-        <option value="5">外島</option>
-    </select>
+    <p>
+        <label for="area">居住區域</label>
+        <select name="area" id="area">
+            <option value="0">請選擇...</option>
+            <option value="1">北部</option>
+            <option value="2">中部</option>
+            <option value="3">南部</option>
+            <option value="4">東部</option>
+            <option value="5">外島</option>
+        </select>
     </p>
     </fieldset>
 <p>
@@ -77,31 +77,33 @@
         <input type="radio" name="place" id="place5" value="1">
         <label for="place5">非常不滿意</label>
 
-        <legend>滿意度</legend>
+        <p>
         <label for="">設備</label>
-        <input type="radio" name="place" id="place1" value="5">
-        <label for="place1">非常滿意</label>
-        <input type="radio" name="place" id="place2" value="4">
-        <label for="place2">滿意</label>
-        <input type="radio" name="place" id="place3" value="3">
-        <label for="place3">普通</label>
-        <input type="radio" name="place" id="place4" value="2">
-        <label for="place4">不滿意</label>
-        <input type="radio" name="place" id="place5" value="1">
-        <label for="place5">非常不滿意</label>
+        <input type="radio" name="device" id="device1" value="5">
+        <label for="device1">非常滿意</label>
+        <input type="radio" name="device" id="device2" value="4">
+        <label for="device2">滿意</label>
+        <input type="radio" name="device" id="device3" value="3">
+        <label for="device3">普通</label>
+        <input type="radio" name="device" id="device4" value="2">
+        <label for="device4">不滿意</label>
+        <input type="radio" name="device" id="device5" value="1">
+        <label for="device5">非常不滿意</label>
+        </p>
 
-        <legend>滿意度</legend>
+        <p>
         <label for="">服務</label>
-        <input type="radio" name="place" id="place1" value="5">
-        <label for="place1">非常滿意</label>
-        <input type="radio" name="place" id="place2" value="4">
-        <label for="place2">滿意</label>
-        <input type="radio" name="place" id="place3" value="3">
-        <label for="place3">普通</label>
-        <input type="radio" name="place" id="place4" value="2">
-        <label for="place4">不滿意</label>
-        <input type="radio" name="place" id="place5" value="1">
-        <label for="place5">非常不滿意</label>
+        <input type="radio" name="service" id="service1" value="5">
+        <label for="service1">非常滿意</label>
+        <input type="radio" name="service" id="service2" value="4">
+        <label for="service2">滿意</label>
+        <input type="radio" name="service" id="service3" value="3">
+        <label for="service3">普通</label>
+        <input type="radio" name="service" id="service4" value="2">
+        <label for="service4">不滿意</label>
+        <input type="radio" name="service" id="service5" value="1">
+        <label for="service5">非常不滿意</label>
+        </p>
         </fieldset>
  
    <fieldset>
@@ -161,7 +163,7 @@ if (isset($_POST["submit"])) {
     echo "<p>你的生日:" . $bday ."</p>";
     echo "<p>你的電話:" . $phone ."</p>";
     echo "<p>你居住區域:" . $area ."</p>";
-    
+
     echo "<p>滿意度: 場地: $place , 設備:$device, 服務:$service </p>";
 }
 
